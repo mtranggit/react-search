8297090e9939a2e4f16a3b1b62c46d0dddc7320a
Michael Trang <michael.trang@hotmail.com>
2018-04-28 11:41:18 +1000

    first commit

---
+++ src/index.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+const Index = () => {
+  return (
+    <div>Hello React with Webpack 4!</div>
+  )
+}
+
+ReactDOM.render(<Index />, document.getElementById("index"));
